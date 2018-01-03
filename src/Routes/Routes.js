3cae83c605f9832f952f89c49755569b58f1e595
Seneca Sisneros <seneca.sisneros@yahoo.com>
2018-01-03 14:19:51 -0800

    added footer design and css

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 import Layout from "../components/Layout";
 import Navbar from "../components/Navbar";
 import ProductDisplay from "../components/ProductDisplay";
 import Story from "../components/Story";
-import Contact from "../components/Contact";
 
 const Routes = () => {
   return (
@@ -18,6 +19,7 @@ const Routes = () => {
           <Route path="/contact" component={Contact} />
           <Route path="/products" component={ProductDisplay} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
